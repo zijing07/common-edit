1e1f9a71b34eb822a1205940f6fe9c6b08c4c60d
zhao <zijing.zhao@aldiko.com>
2012-12-02 14:05:07 +0800

    Raw edition. Need to optimize: - Not handle all the key strokes in the editor (eliminate ctrl, alt, etc.) - Change cursor when over editor area - Add CSS for editor frame - Delete the redundancy data after combine all the data.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,19 +1,50 @@
-
+var DEBUG = true;
 
 var socket = io.connect("/");
-console.log("We have connected");
+if (DEBUG) {
+    console.log("We have connected");
+}
 
+var dmp = new diff_match_patch();
+var origin_data = '';
 
 socket.on('connect', function() {
-    socket.on('message', function(msg) {
-	var elem = document.getElementById('doc');
-	console.log("Message received: " + msg);
-	elem.value = msg;
+    socket.on('message', function(diff) {
+	if (DEBUG) {
+	    console.log("Message received: " + diff);
+	}
+	var msg = CKEDITOR.instances.doc.getData();
+	var patches = dmp.patch_make(msg, diff);
+	var text = dmp.patch_apply(patches, msg);
+	if (DEBUG) {
+	    console.log("Apply changes from " + msg + " to " + text[0]);
+	}
+	CKEDITOR.instances.doc.setData(text[0]);
+	origin_data = text[0];
     });
 });
 
-function send() {
-    var elem = document.getElementById('doc');
-    console.log(elem.value);
-    socket.send(elem.value);
+function send(text) {
+    if (DEBUG) {
+	console.log("Sending: " + text);
+    }
+    socket.send(text);
 }
+
+function handle_text() {
+    var text = CKEDITOR.instances.doc.getData();
+    var diff = dmp.diff_main(origin_data, text);
+    if (DEBUG) {
+	console.log(diff);
+    }
+
+    origin_data = text;
+    send(diff);
+}
+
+window.onload = function() {
+//    CKEDITOR.disableAutoInline = true;
+//    CKEDITOR.inline(document.getElementById('doc'));
+    console.log("Set origin data when first load.");
+    origin_data = CKEDITOR.instances.doc.getData();
+};
