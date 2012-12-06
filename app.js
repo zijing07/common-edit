66ff59b70a1a3dc6f54917f16f11684833ff313f
zhao <zijing.zhao@aldiko.com>
2012-12-07 00:29:07 +0800

    Add interfaces for DB interacting. - Set selection to be done - Optimize UI

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var DEBUG = true;
+
 var express = require('express')
 , app = express()
 , routes = require('./routes')
@@ -7,6 +9,8 @@ var express = require('express')
 
 server.listen(3000);
 
+var store = new express.session.MemoryStore;
+
 app.configure(function(){
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
@@ -15,22 +19,73 @@ app.configure(function(){
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session({ secret:'secret', store:store }));
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
+function getDocDataFromDB(doc_id) {
+    // TODO: mongo db logic
+    doc_data = "Click here to start editting";
+    return doc_data;
+}
 
-app.get('/', routes.index);
+app.get('/doc/:doc_id', function(req, res) {
+    // TODO: Get user id from session
+    req.session.user_id = 1;
 
-var chat = io.of('/chat');
-chat.on('connection', function(socket) {
-    console.log("Connection to chat");
-
-    socket.on("message", function(msg) {
-	console.log("Message received on chat: " + msg);
-	socket.broadcast.send('hi all chat mem');
+    var doc_id = req.params.doc_id;
+    var user_id = req.session.user_id;
+    var doc_data = getDocDataFromDB(doc_id);
+    res.render('doc.ejs', {
+	locals: {
+	    title : 'Common Edit',
+	    doc_data : doc_data,
+	    user_id : user_id
+	}
     });
 });
+
+function addOnlineUser(users, client) {
+    var user = [client.addresss, client.port];
+    users.push(user);
+    
+    return users;
+}
+
+function removeOnlineUser(users, client) {
+    var user = [client.address, client.port];
+    var index = users.indexOf(user);
+    users.splice(index,1);
+
+    return users;
+}
+
+(function () {
+    var doc_socket = io.of("/doc/123");
+    users = [];
+    
+    doc_socket.on('connection', function(socket) {
+	users = addOnlineUser(users, socket.handshake.address);
+
+	if (DEBUG) {
+	    console.log('Online users: ' + users);
+	}
+
+	socket.on("message", function(msg) {
+	    console.log("Message received on chat: " + msg);
+	    socket.broadcast.send(msg);
+	});
+
+	socket.on('disconnect', function() {
+	    removeOnlineUser(users, socket.handshake.address);
+	    if (DEBUG) {
+		console.log('Online users: ' + users);
+	    }
+	});
+    });
+})()
 
 /*
 io.sockets.on('connection', function (socket) {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,62 +1,63 @@
 var DEBUG = true;
 
-// var socket = io.connect("/");
-// if (DEBUG) {
-//     console.log("We have connected");
-// }
+var doc_socket = io.connect('/doc/123');
 
-// var dmp = new diff_match_patch();
-// var origin_data = '';
+var dmp = new diff_match_patch();
+var origin_data = '';
 
-// socket.on('connect', function() {
-//     socket.on('message', function(diff) {
-// 	if (DEBUG) {
-// 	    console.log("Message received: " + diff);
-// 	}
-// 	var msg = CKEDITOR.instances.doc.getData();
-// 	var patches = dmp.patch_make(msg, diff);
-// 	var text = dmp.patch_apply(patches, msg);
-// 	var final_text = text[0].replace(/(<p>)?([,-]*[01],*)+(<\/p>)?\s*/g, "");
-// 	if (DEBUG) {
-// 	    console.log("Apply changes from " + msg + " to " + final_text);
-// 	}
-// 	CKEDITOR.instances.doc.setData(final_text);
-// 	origin_data = text[0];
-//     });
-// });
-
-// function send(text) {
-//     if (DEBUG) {
-// 	console.log("Sending: " + text);
-//     }
-//     socket.send(text);
-// }
-
-// function handle_text() {
-//     var text = CKEDITOR.instances.doc.getData();
-//     var diff = dmp.diff_main(origin_data, text);
-//     if (DEBUG) {
-// 	console.log(diff);
-//     }
-
-//     origin_data = text;
-//     send(diff);
-// }
-
-
-var chat = io.connect('/chat');
-var news = io.connect('/news');
-
-chat.on('connect', function () {
-    chat.send('hi!');
-    console.log('console log working well');
+doc_socket.on('connect', function() {
+    if (DEBUG) {
+	console.log("Doc socket has connected!");
+    }
 });
 
-chat.on('message', function(msg) {
-    console.log('Chat receive res: ' + msg);
+doc_socket.on('message', function(diff) {
+    if (DEBUG) {
+	console.log("Message received: " + diff);
+    }
+    var msg = CKEDITOR.instances.doc.getData();
+    var patches = dmp.patch_make(msg, diff);
+    var text = dmp.patch_apply(patches, msg);
+    var final_text = text[0].replace(/(<p>)?([,-]*[01],*)+(<\/p>)?\s*/g, "");
+    if (DEBUG) {
+	console.log("Apply changes from " + msg + " to " + final_text);
+    }
+    CKEDITOR.instances.doc.setData(final_text);
+    origin_data = text[0];
 });
+
+
+function send(text) {
+    if (DEBUG) {
+	console.log("Sending: " + text);
+    }
+    doc_socket.send(text);
+}
+
+function handle_text() {
+    var text = CKEDITOR.instances.doc.getData();
+    var diff = dmp.diff_main(origin_data, text);
+    if (DEBUG) {
+	console.log(diff);
+    }
+
+    /*
+    var dom_elem = CKEDITOR.dom.element.get(document.getElementById('doc'));
+    document.getElementById('plain_text').innerHTML = dom_elem.getText();
+    */
+
+    var editor = CKEDITOR.instances.doc;
+    var s = editor.getSelection();
+    var range = s.getRanges()[0]; // save selected range
+    range.startOffset = 1;
+    range.endOffset = 1;
+    range.select();
+
+    origin_data = text;
+    send(diff);
+}
 
 window.onload = function() {
-//    console.log("Set origin data when first load.");
-//    origin_data = CKEDITOR.instances.doc.getData();
+    origin_data = CKEDITOR.instances.doc.getData();
+    console.log("Set origin data when first load: " + origin_data);
 };
