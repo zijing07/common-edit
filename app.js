c2396f5040ce69736501e33aa0fd5f5faa29cb4c
zhao <zijing.zhao@aldiko.com>
2012-12-08 00:04:48 +0800

    Fix: - always keep the right editting position - show online users - ready interfaces for LUO CHEN to implement :)

--- app.js
+++ app.js
@@ -27,72 +27,86 @@ app.configure(function(){
 
 function getDocDataFromDB(doc_id) {
     // TODO: mongo db logic
-    doc_data = "Click here to start editting";
+    var doc_data = "Click here to start editting";
     return doc_data;
+}
+
+function getUserNameFromDB(user_id) {
+    // TODO: mongo db logic
+    var user_name = 'test_name';
+    return user_name;
+}
+
+function saveDocData(data) {
+    // TODO: mongo db logic
 }
 
 app.get('/doc/:doc_id', function(req, res) {
     // TODO: Get user id from session
     req.session.user_id = 1;
+    new_doc_socket(req.params.doc_id);
 
     var doc_id = req.params.doc_id;
     var user_id = req.session.user_id;
     var doc_data = getDocDataFromDB(doc_id);
+    var user_name = getUserNameFromDB(user_id);
     res.render('doc.ejs', {
 	locals: {
 	    title : 'Common Edit',
 	    doc_data : doc_data,
-	    user_id : user_id
+	    user_id : user_id,
+	    doc_id : doc_id,
+	    user_name : user_name
 	}
     });
 });
 
-function addOnlineUser(users, client) {
-    var user = [client.addresss, client.port];
+function addOnlineUser(users, client, user_name) {
+    var user = [client.addresss, client.port, user_name];
     users.push(user);
     
     return users;
 }
 
-function removeOnlineUser(users, client) {
-    var user = [client.address, client.port];
+function removeOnlineUser(users, client, user_name) {
+    var user = [client.address, client.port, user_name];
     var index = users.indexOf(user);
     users.splice(index,1);
 
     return users;
 }
 
-(function () {
-    var doc_socket = io.of("/doc/123");
+function new_doc_socket(doc_id) {
+    var doc_url = '/doc/' + doc_id;
+    var doc_socket = io.of(doc_url);
     users = [];
+    var user_name = '';
     
     doc_socket.on('connection', function(socket) {
-	users = addOnlineUser(users, socket.handshake.address);
-
-	if (DEBUG) {
-	    console.log('Online users: ' + users);
-	}
 
 	socket.on("message", function(msg) {
 	    console.log("Message received on chat: " + msg);
 	    socket.broadcast.send(msg);
 	});
 
+	socket.on('user_id', function(data) {
+	    user_name = getUserNameFromDB(data.user_id);
+	    users = addOnlineUser(users, socket.handshake.address, user_name);
+
+	    socket.emit('new_user', { users: users });
+	    socket.broadcast.emit('new_user', { users: users });
+	    
+	});
+
+	socket.on('save_doc_data', function (data) {
+	    saveDocData(data.doc_data);
+	}
+
 	socket.on('disconnect', function() {
-	    removeOnlineUser(users, socket.handshake.address);
-	    if (DEBUG) {
-		console.log('Online users: ' + users);
-	    }
+	    removeOnlineUser(users, socket.handshake.address, user_name);
+	    socket.emit('offline_user', {users: users});
+	    socket.broadcast.emit('offline_user', {users: users});
 	});
     });
-})()
+}
 
-/*
-io.sockets.on('connection', function (socket) {
-
-    socket.on("message", function(msg) {
-	console.log("Message received: " + msg);
-	socket.broadcast.send(msg);
-    });
-});
-*/
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,63 +1,121 @@
 var DEBUG = true;
 
-var doc_socket = io.connect('/doc/123');
+var doc_url = '/doc/' + document.getElementById('doc_id').innerHTML;
+var doc_socket = io.connect(doc_url);
 
 var dmp = new diff_match_patch();
 var origin_data = '';
 
+var before_length = 10;
+function get_before_text(text, offset) {
+    // Get at most 10 characters before it
+    // TODO: special cases handling :|
+
+    var from = offset - before_length;
+    if (from < 0) from = 0;
+
+    return text.substring(from, offset);
+}
+
+function get_offset_position(org_text, new_text, offset) {
+    if (offset == 0) {
+	return 0;
+    }
+    var before_text = get_before_text(org_text, offset);
+    var position = new_text.search(before_text);
+    if (position == -1) position = 0;
+    return position + before_text.length;
+}
+
+function set_editor_data(final_text) {
+    var editor = CKEDITOR.instances.doc;
+    var dom_elem = CKEDITOR.dom.element.get(document.getElementById('doc'));
+
+    // Retrieve original cursor position
+    var range = editor.getSelection().getRanges()[0];
+    var origin_offset = range.startOffset;
+    var origin_plain_text = dom_elem.getText();
+
+    // Apply new text data
+    editor.setData(final_text);
+    var new_plain_text = dom_elem.getText();
+
+    // New cursor position
+    var offset = get_offset_position(origin_plain_text, new_plain_text, range.startOffset);
+    if (DEBUG) {
+	console.log('New offset position is: ' + new_plain_text[offset] + ' ' + offset);
+    }
+
+    editor = CKEDITOR.instances.doc;
+    range = editor.getSelection().getRanges()[0];
+    range.startOffset = offset;
+    range.endOffset = offset;
+    range.select();
+}
+
+function handle_text(event) {
+    var key_code = event.keyCode;
+    if ((key_code >= 65 && key_code <= 90) ||
+	(key_code >= 48 && key_code <= 57) ||
+	(key_code >= 186 && key_code <= 192) ||
+	(key_code >= 219 && key_code <= 222) ||
+	key_code == 8 || key_code == 32) {
+	var text = CKEDITOR.instances.doc.getData();
+	var diff = dmp.diff_main(origin_data, text);
+
+	origin_data = text;
+	doc_socket.send(text);
+    }
+}
+
+function update_user_list(users) {
+    if (DEBUG)
+	console.log(users);
+    var elem = document.getElementById('user_list');
+    elem.innerHTML = '';
+    for (var i=0; i<users.length; ++i) {
+	elem.innerHTML += '<p>' + users[i][0] + ':' + users[i][1] + ':' + users[i][2] + '</p>';
+    }
+}
+
+function save_doc() {
+    doc_socket.emit('save_doc_data', {doc_data, CKEDITOR.instances.doc.getData()});
+}
+
+/***********************************************
+ * socket operations
+ */
 doc_socket.on('connect', function() {
     if (DEBUG) {
 	console.log("Doc socket has connected!");
     }
+    doc_socket.emit('user_id', { user_id: document.getElementById('user_id').innerHTML} );
+});
+
+doc_socket.on('new_user', function(data) {
+    if (DEBUG) {
+	console.log('new user is coming');
+    }
+    update_user_list(data.users);
+});
+
+doc_socket.on('offline_user', function(data) {
+    if (DEBUG) {
+	console.log('a user gets offline');
+    }
+    update_user_list(data.users);
 });
 
 doc_socket.on('message', function(diff) {
-    if (DEBUG) {
-	console.log("Message received: " + diff);
-    }
     var msg = CKEDITOR.instances.doc.getData();
     var patches = dmp.patch_make(msg, diff);
     var text = dmp.patch_apply(patches, msg);
     var final_text = text[0].replace(/(<p>)?([,-]*[01],*)+(<\/p>)?\s*/g, "");
-    if (DEBUG) {
-	console.log("Apply changes from " + msg + " to " + final_text);
-    }
-    CKEDITOR.instances.doc.setData(final_text);
+
+    set_editor_data(final_text);
     origin_data = text[0];
 });
 
-
-function send(text) {
-    if (DEBUG) {
-	console.log("Sending: " + text);
-    }
-    doc_socket.send(text);
-}
-
-function handle_text() {
-    var text = CKEDITOR.instances.doc.getData();
-    var diff = dmp.diff_main(origin_data, text);
-    if (DEBUG) {
-	console.log(diff);
-    }
-
-    /*
-    var dom_elem = CKEDITOR.dom.element.get(document.getElementById('doc'));
-    document.getElementById('plain_text').innerHTML = dom_elem.getText();
-    */
-
-    var editor = CKEDITOR.instances.doc;
-    var s = editor.getSelection();
-    var range = s.getRanges()[0]; // save selected range
-    range.startOffset = 1;
-    range.endOffset = 1;
-    range.select();
-
-    origin_data = text;
-    send(diff);
-}
-
 window.onload = function() {
     origin_data = CKEDITOR.instances.doc.getData();
-    console.log("Set origin data when first load: " + origin_data);
 };
